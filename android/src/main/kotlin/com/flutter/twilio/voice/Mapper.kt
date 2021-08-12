c22f9fef68d9217ae353e7053e2c6cebd19c2746
JoshanPC <joshan.tandukar@yahoo.com>
2021-08-12 18:02:50 +0545

    Returning data from handle message

--- android/src/main/kotlin/com/flutter/twilio/voice/Mapper.kt
+++ android/src/main/kotlin/com/flutter/twilio/voice/Mapper.kt
@@ -2,6 +2,7 @@ package com.flutter.twilio.voice
 
 import com.twilio.voice.*
 import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
 
 object Mapper
@@ -109,24 +110,47 @@ object Mapper
     fun callInviteToMap(message: CallInvite): Map<String, Any?>
     {
         var temp: String = message.customParameters["channel_info"] as String
-        temp = temp.replace("'","\"")
-        temp = temp.replace("None","null")
+        temp = temp.replace("'", "\"")
+        temp = temp.replace("None", "null")
         return mapOf(
                 "twi_call_sid" to message.callSid,
                 "twi_to" to message.to,
                 "twi_from" to message.from,
                 "customParameters" to message.customParameters,
-                "channelInfo" to JSONObject(temp)
+                "channelInfo" to toMap(JSONObject(temp))
         )
     }
 
-    fun customParamsToMap(message: CallInvite): Map<String, Any?> {
-        return mapOf(
-                "twi_call_sid" to message.callSid,
-                "twi_to" to message.to,
-                "twi_from" to message.from,
-                "customParameters" to message.customParameters
-        )
+    @Throws(JSONException::class)
+    fun toMap(jsonobj: JSONObject): Map<String, Any> {
+        val map: MutableMap<String, Any> = HashMap()
+        val keys = jsonobj.keys()
+        while (keys.hasNext()) {
+            val key = keys.next()
+            var value = jsonobj[key]
+            if (value is JSONArray) {
+                value = toList(value)
+            } else if (value is JSONObject) {
+                value = toMap(value)
+            }
+            map[key] = value
+        }
+        return map
+    }
+
+    @Throws(JSONException::class)
+    fun toList(array: JSONArray): List<Any> {
+        val list: MutableList<Any> = ArrayList()
+        for (i in 0 until array.length()) {
+            var value = array[i]
+            if (value is JSONArray) {
+                value = toList(value)
+            } else if (value is JSONObject) {
+                value = toMap(value)
+            }
+            list.add(value)
+        }
+        return list
     }
 
     fun cancelledCallInviteToMap(message: CancelledCallInvite): Map<String, Any?> {
