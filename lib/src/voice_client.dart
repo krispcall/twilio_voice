d54625c827e460f7651238ca85c40ad3413ef558
JoshanPC <joshan.tandukar@yahoo.com>
2021-08-12 17:08:49 +0545

    Data return from handle message

--- lib/src/voice_client.dart
+++ lib/src/voice_client.dart
@@ -433,6 +433,7 @@ class VoiceClient {
         var customParameters = data['data']['customParameters'] as Map<dynamic, dynamic>;
         String temp = data['data']['customParameters']['channel_info'] as String;
         temp = temp.replaceAll("'","\"");
+        temp = temp.replaceAll("None","");
         var channelInfo = (json.decode(temp)) as Map<dynamic, dynamic>;
         print("$TAG onCallInvite "+json.encode(customParameters));
         print("$TAG onCallInvite "+json.encode(channelInfo));
@@ -451,6 +452,7 @@ class VoiceClient {
         var customParameters = data['data']['customParameters'] as Map<dynamic, dynamic>;
         String temp = data['data']['customParameters']['channel_info'] as String;
         temp = temp.replaceAll("'","\"");
+        temp = temp.replaceAll("None","");
         var channelInfo = (json.decode(temp)) as Map<dynamic, dynamic>;
         print("$TAG onCancelledCallInvite "+json.encode(customParameters));
         print("$TAG onCancelledCallInvite "+json.encode(channelInfo));
