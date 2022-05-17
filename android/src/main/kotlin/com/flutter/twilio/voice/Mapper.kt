1abb65b7722c374cba79c9c504663733882024d4
Sushan Dristi <dristisushan@gmail.com>
2022-05-17 08:06:27 +0545

    fix temp

--- android/src/main/kotlin/com/flutter/twilio/voice/Mapper.kt
+++ android/src/main/kotlin/com/flutter/twilio/voice/Mapper.kt
@@ -47,7 +47,7 @@ object Mapper
         temp = temp.replace(", '", ", \"");
         temp = temp.replace("'}", "\"}");
         temp = temp.replace("None", "null")
-        print("this is temp $temp1");
+        print("this is temp $temp 1");
         return mapOf(
                 "twi_call_sid" to message.callSid,
                 "twi_to" to message.to,
